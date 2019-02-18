7aa68b8ead867fa9f3846823e16b70a52bd67a97
aisim <github@mismail.xyz>
2019-02-18 09:36:54 +0100

    created new model with hvad

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.urls import reverse
+from hvad.models import TranslatableModel, TranslatedFields
+
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
     )
-    body = models.TextField()
+
+    translations = TranslatedFields(
+        title = models.CharField(max_length=200),
+        body = models.TextField(),
+    )
+
 
     def __str__(self):
         return self.title
